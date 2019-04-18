dad4d3c29e840126cddebc810a72217305475d14
Jessica Minnier <jessica.minnier@gmail.com>
2019-04-18 08:40:48 -0700

    move meike's slides into origial document. add archive

--- 02-data-wrangling-tidyverse/02_data_wrangling_slides.R
+++ 02-data-wrangling-tidyverse/02_data_wrangling_slides.R
@@ -43,7 +43,7 @@ mono_light(
 )
 
 ## ---- eval=FALSE---------------------------------------------------------
-## y <- iris %>% count(Species)
+## y <- 5
 ## y
 
 ## ------------------------------------------------------------------------
@@ -67,27 +67,18 @@ mydata_tib <- read_csv("data/small_data.csv")
 mydata_tib
 
 ## ---- results=FALSE------------------------------------------------------
-mydata <- read_csv("data/small_data.csv")
-mydata
-glimpse(mydata)
-str(mydata)
-head(mydata)
-summary(mydata)
-class(mydata)
+glimpse(mydata_df)
+str(mydata_df)       # How are glimpse() and str() different?
+head(mydata_df)
+summary(mydata_df)
+class(mydata_df)     # What information does class() give?
 
-## ------------------------------------------------------------------------
-untidy_data <- tibble(
-  name = c("Ana","Bob","Cara"),
-  wt_07_01_2018 = c(100, 150, 140),
-  wt_08_01_2018 = c(104, 155, 138),
-  wt_09_01_2018 = c(NA, 160, 142)
-)
-untidy_data
-
-## ------------------------------------------------------------------------
-untidy_data %>% gather(key = "date", value = "weight", -name) %>%
-  mutate(date = str_remove(date,"wt_"),
-         date = dmy(date))
+## ---- results=FALSE------------------------------------------------------
+glimpse(mydata_tib)
+str(mydata_tib)       
+head(mydata_tib)
+summary(mydata_tib)
+class(mydata_tib)     
 
 ## ------------------------------------------------------------------------
 untidy_data <- tibble(
@@ -101,6 +92,141 @@ untidy_data %>%
   separate(col = meds, into = c("med_name","dose_mg","times_per_day"), sep=" ") %>%
   mutate(times_per_day = as.numeric(str_remove(times_per_day, "xday")),
          dose_mg = as.numeric(str_remove(dose_mg, "mg")))
+
+## ------------------------------------------------------------------------
+untidy_data2 <- tibble(
+  name = c("Ana","Bob","Cara"),
+  wt_07_01_2018 = c(100, 150, 140),
+  wt_08_01_2018 = c(104, 155, 138),
+  wt_09_01_2018 = c(NA, 160, 142)
+)
+untidy_data2
+
+## ------------------------------------------------------------------------
+untidy_data2 %>% 
+  gather(key = "date", value = "weight", -name) %>%
+  mutate(date = str_remove(date,"wt_"),
+         date = dmy(date))     # dmy() is a function in the lubridate package
+
+## ------------------------------------------------------------------------
+mydata_tib %>% head(n=3)      # prounounce %>% as "then"
+
+## ---- eval=FALSE---------------------------------------------------------
+## mydata_tib %>% head(n=3) %>% summary()
+
+## ---- results=FALSE------------------------------------------------------
+demo_data <- read_csv("data/yrbss_demo.csv")
+
+## ------------------------------------------------------------------------
+demo_data %>% filter(bmi > 20)
+
+## ------------------------------------------------------------------------
+demo_data[demo_data$grade=="9th",]
+
+## ------------------------------------------------------------------------
+demo_data %>% filter(grade=="9th")
+
+## ---- eval=FALSE---------------------------------------------------------
+## demo_data %>% filter(bmi < 5)
+## demo_data %>% filter(bmi/stweight < 0.5)    # can do math
+## demo_data %>% filter((bmi < 15) | (bmi > 50))
+## demo_data %>% filter(bmi < 20, stweight < 50, sex == "Female") # filter on multiple variables
+## 
+## demo_data %>% filter(record == 506901)      # note the use of == instead of just =
+## demo_data %>% filter(sex == "Male")
+## demo_data %>% filter(!(grade == "9th"))
+## demo_data %>% filter(grade %in% c("10th", "11th"))
+## 
+## demo_data %>% filter(is.na(bmi))
+## demo_data %>% filter(!is.na(bmi))
+
+## ------------------------------------------------------------------------
+demo_data %>% select(record, grade)
+
+## ------------------------------------------------------------------------
+demo_data[, c("record","age","sex")]
+
+## ---- results="hold"-----------------------------------------------------
+demo_data %>% select(record, age, sex)
+demo_data %>% select(record:sex)
+
+## ---- eval=FALSE---------------------------------------------------------
+## demo_data %>% select(record:sex)
+## demo_data %>% select(one_of(c("age","stweight")))
+## 
+## demo_data %>% select(-grade,-sex)
+## demo_data %>% select(-(record:sex))
+## 
+## demo_data %>% select(contains("race"))
+## demo_data %>% select(starts_with("r"))
+## demo_data %>% select(-contains("r"))
+## 
+## demo_data %>% select(record, race4, race7, everything())
+
+## ------------------------------------------------------------------------
+demo_data %>% rename(id = record)   # order: new_name = old_name
+
+## ---- include=FALSE------------------------------------------------------
+newdata <- demo_data %>%
+  # rename_all(toupper) %>%
+  select_if(is.character) %>%
+  filter(race7 %in% c("Asian","Native Hawaiian/other PI"), grade == "9th") %>%
+  filter(age != "12 years old or younger") %>%
+  select(-race4)
+dim(newdata)
+
+## ------------------------------------------------------------------------
+newdata <- demo_data %>% 
+  mutate(height_m = sqrt(stweight / bmi))   # use = (not <- or ==) to define new variable
+
+newdata %>% select(record, bmi, stweight)
+
+## ---- eval=FALSE---------------------------------------------------------
+## demo_data %>% mutate(bmi_high = (bmi > 30))
+## 
+## demo_data %>% mutate(male = (sex == "Male"))
+## demo_data %>% mutate(male = 1 * (sex == "Male"))
+## 
+## demo_data %>% mutate(grade_num = as.numeric(str_remove(grade, "th")))
+
+## ------------------------------------------------------------------------
+demo_data2 <- demo_data %>%
+  mutate(
+    bmi_group = case_when(
+      bmi < 18.5 ~ "underweight",               # condition ~ new_value
+      bmi >= 18.5 & bmi <= 24.9 ~ "normal",
+      bmi > 24.9 & bmi <= 29.9 ~ "overweight",
+      bmi > 29.9 ~ "obese")
+    )
+demo_data2 %>% select(bmi, bmi_group) %>% head()
+
+## ------------------------------------------------------------------------
+demo_data %>% na.omit()
+
+## ------------------------------------------------------------------------
+data_dups <- tibble(
+  name = c("Ana","Bob","Cara", "Ana"),
+  race = c("Hispanic","Other", "White", "Hispanic")
+)
+
+## ------------------------------------------------------------------------
+data_dups
+
+## ------------------------------------------------------------------------
+data_dups %>% distinct()
+
+## ------------------------------------------------------------------------
+demo_data %>% arrange(bmi, stweight) %>% head(n=3)
+
+demo_data %>% arrange(desc(bmi), stweight) %>% head(n=3)
+
+## ----eval=FALSE----------------------------------------------------------
+## demo_data %>% select_if(is.numeric)
+## 
+## demo_data %>% rename_all(toupper) # toupper() is a function
+## demo_data %>% rename_if(is.character, toupper)
+## 
+## demo_data %>% rename_at(vars(contains("race")), toupper)
 
 ## ---- eval=FALSE---------------------------------------------------------
 ## h(g(f(mydata)))
@@ -117,117 +243,13 @@ untidy_data %>%
 ##   h()
 
 ## ------------------------------------------------------------------------
-mydata_new <- mydata %>% select(id, weight_kg, bmi) %>%
+mydata_new <- mydata_tib %>% select(id, weight_kg, bmi) %>%
   mutate(height_m = sqrt(weight_kg /bmi))
 mydata_new %>% head(n=3)
-
-## ---- results=FALSE------------------------------------------------------
-demo_data <- read_csv("data/yrbss_demo.csv")
-qn_data <- read_csv("data/yrbss_qn.csv")
-
-glimpse(demo_data)
-glimpse(qn_data)
-
-## ---- eval = FALSE-------------------------------------------------------
-## select(.data = demo_data, "record")
-## select(demo_data, "record")
-## select(demo_data, record)
-## demo_data %>% select(record)
-
-## ---- echo = FALSE-------------------------------------------------------
-demo_data %>% select(record)
-
-## ------------------------------------------------------------------------
-demo_data %>% filter(bmi > 20)
-
-## ------------------------------------------------------------------------
-demo_data[demo_data$grade=="9th",]
-
-## ------------------------------------------------------------------------
-demo_data %>% filter(grade=="9th")
-
-## ---- eval=FALSE---------------------------------------------------------
-## demo_data %>% filter(record==506901)
-## demo_data %>% filter(sex=="Male")
-## demo_data %>% filter(grade %in% c("10th","11th"))
-## demo_data %>% filter(!(grade=="9th"))
-## demo_data %>% filter(bmi < 20, stweight < 50, sex=="Female") # filter on multiple
-## demo_data %>% filter(is.na(bmi))
-## demo_data %>% filter(!is.na(bmi))
-## demo_data %>% filter(bmi < 5)
-## demo_data %>% filter(bmi/stweight < 0.5) # can do math
-## demo_data %>% filter((bmi<15)|(bmi>50))
-
-## ------------------------------------------------------------------------
-demo_data %>% select(record, grade)
-
-## ------------------------------------------------------------------------
-demo_data[, c("record","age","sex")]
-
-## ---- results="hold"-----------------------------------------------------
-demo_data %>% select(record, age, sex)
-demo_data %>% select(record:sex)
-
-## ---- eval=FALSE---------------------------------------------------------
-## demo_data %>% select(-grade,-sex)
-## demo_data %>% select(record:sex)
-## demo_data %>% select(-(record:sex))
-## demo_data %>% select(contains("race"))
-## demo_data %>% select(record, race4, race7, everything())
-## demo_data %>% select(one_of(c("age","stweight")))
-## demo_data %>% select(starts_with("r"))
-## demo_data %>% select(-contains("r"))
-## demo_data %>% select(1:3)
-
-## ------------------------------------------------------------------------
-demo_data %>% rename(id = record)
-
-## ----eval=FALSE----------------------------------------------------------
-## demo_data %>% select_if(is.numeric)
-## demo_data %>% rename_if(is.character, toupper) # toupper() is a function
-## demo_data %>% rename_all(toupper)
-## demo_data %>% rename_at(vars(contains("race")), toupper)
-## demo_data %>% rename_if(is.numeric, funs(paste0(.,"_num")))
-
-## ---- include=FALSE------------------------------------------------------
-newdata <- demo_data %>%
-  rename_all(toupper) %>%
-  select_if(is.character) %>%
-  filter(RACE7%in%c("Asian","Native Hawaiin/other PI"), GRADE=="9th") %>%
-  filter(AGE!="12 years old or younger") %>%
-  select(-RACE4)
-dim(newdata)
-
-## ------------------------------------------------------------------------
-newdata <- demo_data %>% 
-  select(record, bmi:stweight) %>%
-  mutate(height_m = sqrt(stweight /bmi))
-newdata
-
-## ---- eval=FALSE---------------------------------------------------------
-## demo_data %>% mutate(bmi_high = bmi > 30)
-## demo_data %>% mutate(male = 1*(sex=="Male"))
-## demo_data %>% mutate(grade_num = as.numeric(str_remove(grade,"th")))
-
-## ------------------------------------------------------------------------
-
-demo_data2 <- demo_data %>%
-  mutate(
-    age_int = case_when(
-      age=="12 years old or younger" ~ 12,
-      age=="18 years old or older" ~ 18,
-      TRUE ~ as.numeric(str_remove(age, " years old"))
-  ))
-demo_data2 %>% tabyl(age,age_int)
-
-## ------------------------------------------------------------------------
-demo_data %>% distinct()
-
-## ------------------------------------------------------------------------
-demo_data %>% na.omit()
 
 ## ---- eval=FALSE, echo=FALSE---------------------------------------------
 ## # RUN THESE AFTER KNITTING
 ## knitr::purl(here::here("02-data-wrangling-tidyverse/02_data_wrangling_slides.Rmd"), out = here::here("02-data-wrangling-tidyverse/02_data_wrangling_slides.R"))
-## pagedown::chrome_print(here::here("02-data-wrangling-tidyverse/02_data_wrangling_slides.Rmd"))
+## # remotes::install_github('rstudio/pagedown')
+## pagedown::chrome_print(here::here("02-data-wrangling-tidyverse/02_data_wrangling_slides.html"))
 
