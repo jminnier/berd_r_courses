a685553e881d97c95cb5cda9a8c6b2dc0c2527e1
Jessica Minnier <jessica.minnier@gmail.com>
2022-08-10 09:11:37 -0700

    update instructions

--- 01-install-packages.R
+++ 01-install-packages.R
@@ -9,19 +9,26 @@
 # This is the first line of R code, you will see output in the console window
 install.packages("pacman")
 
+# Orange/red colored messages are ok! Ignore the output unless you see "Error"
+
+# We are installing the pacman package which we will use to install more packages.
+
 # This will likely take a long time to run if you do not have tidyverse installed
-pacman::p_load(tidyverse, janitor, tinytex, update = TRUE)
+# or haven't updated in a while
+pacman::p_load(tidyverse, 
+               janitor, 
+               rmarkdown, update = TRUE)
 
 # If you are asked
 # "Do you want to install from sources the package which needs compilation? (Yes/no/cancel) "
-# You can type no in the console
+# You can type "no" (without quotes) in the console window, and press enter
 
+# If you are asked "do you want to use your own personal library" or something to that effect, say "yes"
 
-# For knitting to pdf
+# *OPTIONAL* 
+# For knitting to pdf, you may want to install a tex client
+pacman::p_load(tinytex)
 tinytex::install_tinytex()
 
-# Test some installations by loading packages
-library(tidyverse)
-library(lubridate)
-library(readxl)
-library(janitor)
+
+
